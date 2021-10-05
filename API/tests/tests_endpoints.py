1b7432ce9a4a489fe4d0b80b4682804705c35670
Zijie Xiong <zx811@nyu.edu>
2021-10-05 12:29:53 -0400

    Use envrionment variable for local server and add test for Hello

--- API/tests/tests_endpoints.py
+++ API/tests/tests_endpoints.py
@@ -1,11 +1,19 @@
-from unittest import TestCase, skip
+"""
+This file holds the tests for endpoints.py
+"""
 
+from unittest import TestCase, skip
+from flask_restx import Resource, Api
 import API.endpoints as ep
 
 class EndpointTestCase(TestCase):
     def setUp(self):
         pass;
+
     def tearDown(self):
         pass;
+
     def test_hello(self):
-        self.assertTrue(False);
+        hello = ep.HelloWorld(Resource)
+        ret = hello.get()
+        self.assertIsInstance(ret, dict)
--- local.sh
+++ local.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+HOST=127.0.0.1
+PORT=8000
+ENDPOINT=API/endpoints
+
 # run our server locally:
 PYTHONPATH=$(pwd):$PYTHONPATH
-FLASK_APP=endpoints flask run --host=127.0.0.1 --port=8000
+FLASK_APP=$ENDPOINT flask run --host=$HOST --port=$PORT
